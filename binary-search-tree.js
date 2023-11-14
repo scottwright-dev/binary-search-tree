ef041ea6f177e6229917475833c25737c9f4f2e2
Scott Wright <scott.wright.uk@gmail.com>
2023-11-14 10:02:15 +0000

    refactor: createTree and buildTree functions
    createTree function:
    - Added logic to remove duplicates and sort the array in ascending order before constructing the tree. This ensures that buildTree always works with a sorted, de-duplicated array.
    - Changed return value to directly return the root node of the binary search tree instead of an object containing the root.
    
    buildTree function:
    - Constructs the binary search tree. This function takes a sorted and deduplicated array from createTree function and builds a tree recursively.
    - The function calculates the middle index of the current array to create the root node for each subtree, ensuring a balanced tree structure.
    - It then recursively builds the left and right subtrees by calling itself with the segments of the array.
    - Each recursive call returns the root node of the subtree, with the base case returning null when the start index exceeds the end index, signifying no elements left to add to the tree.
    - The final return of the function is the root node of the entire binary search tree constructed from the provided array.
    
    Test Cases:
    - Simple test cases added in testing area

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -12,10 +12,35 @@ function createNode(data) {
 
 // Tree factory function to create a new binary search tree from an array
 function createTree(array) {
-    // Placeholder for buildTree function which will construct the tree
-    const root = buildTree(array);
-
-    return {
-        root
-    };
+    // Filter out duplicates using the Set method, convert into a new array using spread operator
+    // then call sort on the array to sort in ascending order
+    const sortedArray = [...new Set(array)].sort((a,b) => a - b);
+    // call buildTree to construct the tree from the sorted array and return the root node.
+    // buildTree recursively divides array and creates a node from the middle element
+    const root = buildTree(sortedArray);
+    // Return the root node of the constructed binary search tree.
+    return root;
 }
+
+// Build function which turns the array into a balanced binary tree full of node objects
+function buildTree(array, start = 0, end = array.length -1) { 
+    // if start index is greater than end index, return null. (Termination condition of recursion)
+    if (start > end) return null;
+    // find mid point index
+    const mid = Math.floor((start + end) / 2);
+    // create the root node with this element
+    const rootNode = createNode(array[mid]);
+    // recursively build the left and right subtrees and assign them
+    rootNode.leftChild = (buildTree(array, start, mid -1));
+    rootNode.rightChild = (buildTree(array, mid + 1, end));
+    // Return the root node of this subtree
+    return rootNode;
+
+}
+
+// TESTING AREA
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+console.log("test array is:", testArray);
+
+let testTree = createTree(testArray);
+console.log("testTree:", testTree);
