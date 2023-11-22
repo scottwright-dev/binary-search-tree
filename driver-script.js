df1e06db88ae715a26013144ac6a529cf5875fb9
Scott Wright <scott.wright.uk@gmail.com>
2023-11-22 17:41:23 +0000

    add: driver script for BST testing
    This commit introduces a driver script that acts as a comprehensive testing suite for the binary search tree (BST) implementation.
    
    The script includes the following functionalities:
    
    - Generates a BST from an array of random numbers less than 100.
    - Checks and confirms the balance status of the tree.
    - Performs and prints results of various traversal methods (level-order, pre-order, post-order, and in-order).
    - Unbalances the tree by adding several numbers greater than 100.
    - Confirms the imbalance of the tree.
    - Rebalances the tree and verifies the balance status post-rebalancing.
    - Repeats the traversal methods to confirm the integrity of the tree post-rebalancing.
    
    Additionally this commit links the driver script in the index.html.

---
+++ driver-script.js
@@ -0,0 +1,41 @@
+// generate array of random numbers
+function generateRandomArray(size) {
+    let array = [];
+    for (let i = 0; i < size; i++) {
+        array.push(Math.floor(Math.random() * 100));
+    }
+    return array;
+}
+
+// create a binary search tree from an array of random numbers < 100
+let randomArray = generateRandomArray(10);
+let bst = createTree(randomArray);
+
+// confirm that the tree is balanced
+console.log('Is the tree balanced initially?', isBalanced(bst));
+
+// print out all elements in level, pre, post, and in order
+console.log('Level Order:', levelOrder(bst));
+console.log('Pre Order:', preOrder(bst));
+console.log('Post Order:', postOrder(bst));
+console.log('In Order:', inOrder(bst));
+
+// unbalance the tree by adding several numbers > 100
+bst = insertNode(101, bst);
+bst = insertNode(102, bst);
+bst = insertNode(103, bst);
+
+// confirm that the tree is unbalanced
+console.log('Is the tree balanced after adding elements?', isBalanced(bst));
+
+// balance the tree
+bst = rebalance(bst);
+
+// bonfirm that the tree is balanced
+console.log('Is the tree balanced after rebalancing?', isBalanced(bst));
+
+// print out all elements again in level, pre, post, and in order
+console.log('Level Order after rebalancing:', levelOrder(bst));
+console.log('Pre Order after rebalancing:', preOrder(bst));
+console.log('Post Order after rebalancing:', postOrder(bst));
+console.log('In Order after rebalancing:', inOrder(bst));
